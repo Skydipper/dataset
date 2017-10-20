6c4e3eddb5d166cca9554779d7a3db9fbded7610
Sergio Gordillo <sgordillogallardo@gmail.com>
2017-10-20 11:06:31 +0200

    [fix] remove redis dependency

--- app/src/app.js
+++ app/src/app.js
@@ -51,12 +51,6 @@ const onDbReady = (err) => {
 
     app.use(koaBody);
 
-    app.use(require('microservice-cache-middleware')({
-        prefix: 'dataset',
-        redisUrl: process.env.REDIS_URL
-    }));
-
-
     app.use(async (ctx, next) => {
         try {
             await next();
