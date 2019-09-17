5ea6443eafa338ca6720e8bbc345b1b65a9998b8
stanislavgalin <stasgalin@gmail.com>
2019-09-17 10:35:25 +0300

    add test for delete endpoint with case: tableName is null

--- app/test/e2e/dataset-delete.spec.js
+++ app/test/e2e/dataset-delete.spec.js
@@ -143,6 +143,89 @@ describe('Dataset delete tests', () => {
         getResponse.body.errors[0].should.have.property('detail').and.equal(`Dataset with id '${cartoFakeDataset._id}' doesn't exist`);
     });
 
+    it('Delete an document dataset with missing tableName should delete (happy case)', async () => {
+        const cartoFakeDataset = await new Dataset(createDataset('json', { tableName: null, connectorType: 'document' })).save();
+
+        nock(`${process.env.CT_URL}`)
+            .get(`/v1/dataset/${cartoFakeDataset._id}/layer?protected=true`)
+            .once()
+            .reply(200, {
+                status: 200,
+                data: []
+            });
+
+        nock(`${process.env.CT_URL}`)
+            .get(`/v1/dataset/${cartoFakeDataset._id}/widget?protected=true`)
+            .once()
+            .reply(200, {
+                status: 200,
+                data: []
+            });
+
+        nock(`${process.env.CT_URL}`)
+            .delete(`/v1/dataset/${cartoFakeDataset._id}/vocabulary/knowledge_graph?application=rw`)
+            .once()
+            .reply(200, {
+                status: 200,
+                data: []
+            });
+
+        nock(`${process.env.CT_URL}`)
+            .delete(`/v1/dataset/${cartoFakeDataset._id}/layer`)
+            .once()
+            .reply(200, {
+                status: 200,
+                data: []
+            });
+
+        nock(`${process.env.CT_URL}`)
+            .delete(`/v1/dataset/${cartoFakeDataset._id}/widget`)
+            .once()
+            .reply(200, {
+                status: 200,
+                data: []
+            });
+
+        nock(`${process.env.CT_URL}`)
+            .delete(`/v1/dataset/${cartoFakeDataset._id}/metadata`)
+            .once()
+            .reply(200, {
+                status: 200,
+                data: []
+            });
+
+        nock(`${process.env.CT_URL}`)
+            .delete(`/v1/dataset/${cartoFakeDataset._id}/vocabulary`)
+            .once()
+            .reply(200, {
+                status: 200,
+                data: []
+            });
+
+        const deleteResponse = await requester.delete(`/api/v1/dataset/${cartoFakeDataset._id}?loggedUser=${JSON.stringify(ROLES.ADMIN)}`).send();
+
+        deleteResponse.status.should.equal(200);
+        const createdDataset = deserializeDataset(deleteResponse);
+
+        deleteResponse.status.should.equal(200);
+        deleteResponse.body.should.have.property('data').and.be.an('object');
+        createdDataset.should.have.property('name').and.equal(cartoFakeDataset.name);
+        createdDataset.should.have.property('applicationConfig').and.deep.equal(cartoFakeDataset.applicationConfig);
+        createdDataset.should.have.property('connectorType').and.equal('document');
+        createdDataset.should.have.property('provider').and.equal('json');
+        createdDataset.should.have.property('userId').and.equal(ROLES.ADMIN.id);
+        createdDataset.should.have.property('status').and.equal('saved');
+        createdDataset.should.have.property('overwrite').and.equal(true);
+        createdDataset.legend.should.be.an.instanceOf(Object);
+        createdDataset.clonedHost.should.be.an.instanceOf(Object);
+
+        const getResponse = await requester.get(`/api/v1/dataset/${cartoFakeDataset._id}`).send();
+
+        getResponse.status.should.equal(404);
+        getResponse.body.should.have.property('errors').and.be.an('array');
+        getResponse.body.errors[0].should.have.property('detail').and.equal(`Dataset with id '${cartoFakeDataset._id}' doesn't exist`);
+    });
+
     it('Deleting an existing carto dataset with missing layer MS should fail with a meaningful error', async () => {
         const cartoFakeDataset = await new Dataset(createDataset('cartodb')).save();
 
