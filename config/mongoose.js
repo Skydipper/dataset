bdc4fcdacc422fa12f15dd54ba10a20368888cf8
Tiago Garcia <tiagojsag@gmail.com>
2020-07-13 13:35:03 +0100

    Logging and test CS improvements

--- config/mongoose.js
+++ config/mongoose.js
@@ -8,7 +8,6 @@ const mongooseOptions = {
     readPreference: 'secondaryPreferred', // Has MongoDB prefer secondary servers for read operations.
     appname: 'dataset', // Displays the app name in MongoDB logs, for ease of debug
     serverSelectionTimeoutMS: 5000, // Number of milliseconds the underlying MongoDB driver has to pick a server
-    loggerLevel: config.get('logger.level'), // Logger level to pass to the MongoDB driver
     poolSize: 10 // Default is 5, setting to 10 to prevent slower queries from blocking things
 };
 
